e1149822e5f9ffed3bc8c194471064575c4186f1
LTilio <Leandro.tilio@gmail.com>
2024-05-22 18:43:58 -0300

    Feat: Modelos para ROLE

---
+++ src/main/java/org/serratec/backend/redesocial/model/Perfil.java
@@ -0,0 +1,39 @@
+package org.serratec.backend.redesocial.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Perfil {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_perfil")
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/org/serratec/backend/redesocial/model/Usuario.java
+++ src/main/java/org/serratec/backend/redesocial/model/Usuario.java
@@ -1,9 +1,11 @@
 package org.serratec.backend.redesocial.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -25,163 +27,180 @@ import jakarta.persistence.Table;
 @Table(name = "usuario")
 public class Usuario implements UserDetails, Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+	@Column(nullable = false)
+	private String nome;
 
-    @Column(nullable = false)
-    private String sobrenome;
+	@Column(nullable = false)
+	private String sobrenome;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+	@Column(nullable = false, unique = true)
+	private String email;
 
-    @Column(nullable = false)
-    private String senha;
+	@Column(nullable = false)
+	private String senha;
 
-    @Column(nullable = false)
-    private Date dataNascimento;
+	@Column(nullable = false)
+	private Date dataNascimento;
 
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<UsuarioRelationship> usuarioRelacoes = new HashSet<>();
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<UsuarioRelationship> usuarioRelacoes = new HashSet<>();
 
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<Post> posts = new HashSet<>();
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<Post> posts = new HashSet<>();
 
-    public Usuario() {
-    }
+	@OneToMany(mappedBy = "id.usuario", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Set<UsuarioPerfil> usuarioPerfis = new HashSet<>();
 
-    public Usuario(Long id, String nome, String sobrenome, String email, String senha, Date dataNascimento) {
-        this.id = id;
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.senha = senha;
-        this.dataNascimento = dataNascimento;
-    }
+	public Usuario() {
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Usuario(Long id, String nome, String sobrenome, String email, String senha, Date dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getSobrenome() {
-        return sobrenome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
+	public String getSobrenome() {
+		return sobrenome;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public String getSenha() {
+		return senha;
+	}
 
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
 
-    public Set<UsuarioRelationship> getUsuarioPerfis() {
-        return usuarioRelacoes;
-    }
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
-    public void setUsuarioPerfis(Set<UsuarioRelationship> usuarioPerfis) {
-        this.usuarioRelacoes = usuarioPerfis;
-    }
+	public Set<UsuarioRelationship> getUsuarioRelacoes() {
+		return usuarioRelacoes;
+	}
 
-    public Set<Post> getPosts() {
-        return posts;
-    }
+	public void setUsuarioRelacoes(Set<UsuarioRelationship> usuarioRelacoes) {
+		this.usuarioRelacoes = usuarioRelacoes;
+	}
 
-    public void setPosts(Set<Post> posts) {
-        this.posts = posts;
-    }
+	public Set<Post> getPosts() {
+		return posts;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        for (UsuarioRelationship usuarioRelacoes : usuarioRelacoes) {
-            authorities.add(new SimpleGrantedAuthority(usuarioRelacoes.getRelationship().getId().toString()));
-        }
-        return authorities;
-    }
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
 
-    @Override
-    public String getPassword() {
-        return senha;
-    }
+	public Set<UsuarioPerfil> getUsuarioPerfis() {
+		return usuarioPerfis;
+	}
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
+	public void setUsuarioPerfis(Set<UsuarioPerfil> usuarioPerfis) {
+		this.usuarioPerfis = usuarioPerfis;
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auth = new ArrayList<>();
+		for (UsuarioPerfil usuarioPerfil: getUsuarioPerfis()) {
+			String perfilNome = usuarioPerfil.getId().getPerfil().getNome();
+			if(perfilNome != null && !perfilNome.trim().isEmpty()) {
+				auth.add(new SimpleGrantedAuthority(perfilNome));	
+			}else {
+	            System.out.println("Invalid perfil name: " + perfilNome);
+	        }
+		}
+		return auth;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+	@Override
+	public String getPassword() {
+		return senha;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	@Override
+	public String getUsername() {
+		return email;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Usuario other = (Usuario) obj;
-        return Objects.equals(id, other.id);
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
 }
