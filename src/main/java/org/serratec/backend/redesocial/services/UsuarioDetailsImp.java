c21bf59c584f2ac61ce61a59fbb987a7c7fab0a1
LTilio <Leandro.tilio@gmail.com>
2024-05-22 18:45:05 -0300

    feat: userDetails

---
+++ src/main/java/org/serratec/backend/redesocial/services/UsuarioDetailsImp.java
@@ -0,0 +1,27 @@
+package org.serratec.backend.redesocial.services;
+
+import org.serratec.backend.redesocial.model.Usuario;
+import org.serratec.backend.redesocial.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UsuarioDetailsImp implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.findByEmail(username);
+		if(usuario == null) {
+			throw new RuntimeException();
+		}
+		return usuario;
+	}
+	
+}
