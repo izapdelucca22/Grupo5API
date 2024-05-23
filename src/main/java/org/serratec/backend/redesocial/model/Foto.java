1aae7a07c58c5761968c437601df1fade66421bc
iza.pdelucca <bellapdelucca@gmail.com>
2024-05-23 15:37:30 -0300

    feat: criação classe foto

---
+++ src/main/java/org/serratec/backend/redesocial/model/Foto.java
@@ -0,0 +1,89 @@
+package org.serratec.backend.redesocial.model;
+
+import java.sql.Types;
+
+import org.hibernate.annotations.JdbcTypeCode;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Foto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_foto")
+	private Long id;
+
+	@Lob
+	@JdbcTypeCode(Types.BINARY)
+	private byte[] dados;
+
+	private String tipo;
+
+	private String nome;
+	
+	@OneToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	public Foto() {
+	}
+
+	public Foto(Long id, byte[] dados, String tipo, String nome, Usuario usuario) {
+		super();
+		this.id = id;
+		this.dados = dados;
+		this.tipo = tipo;
+		this.nome = nome;
+		this.usuario = usuario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public byte[] getDados() {
+		return dados;
+	}
+
+	public void setDados(byte[] dados) {
+		this.dados = dados;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
