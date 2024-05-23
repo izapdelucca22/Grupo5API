d39e6b350810c4ec4176968013f4e1c8de7961b8
iza.pdelucca <bellapdelucca@gmail.com>
2024-05-23 15:39:28 -0300

    feat: adição metodos para inserção de foto

--- src/main/java/org/serratec/backend/redesocial/controller/UsuarioController.java
+++ src/main/java/org/serratec/backend/redesocial/controller/UsuarioController.java
@@ -1,26 +1,67 @@
 package org.serratec.backend.redesocial.controller;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.serratec.backend.redesocial.DTO.UsuarioDTO;
+import org.serratec.backend.redesocial.model.Foto;
 import org.serratec.backend.redesocial.model.Usuario;
+import org.serratec.backend.redesocial.service.FotoService;
 import org.serratec.backend.redesocial.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
+
+	@Autowired
+	private FotoService fotoService;
+
+	@GetMapping("/listarDTO")
+	public ResponseEntity<List<UsuarioDTO>> listar() {
+		List<UsuarioDTO> usuarios = usuarioService.listar();
+		return ResponseEntity.ok(usuarios);
+	}
+
+	@GetMapping("/{id}/DTO")
+	public ResponseEntity<UsuarioDTO> buscar(@PathVariable Long id) {
+		UsuarioDTO usuario = usuarioService.buscar(id);
+		return ResponseEntity.ok(usuario);
+	}
+
+	@GetMapping("/{id}/foto")
+	public ResponseEntity<byte[]> buscarFoto(@PathVariable Long id) {
+		Foto foto = fotoService.buscarPorIdUsuario(id);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", foto.getTipo());
+		headers.add("Content-length", String.valueOf(foto.getDados().length));
+
+		return new ResponseEntity<>(foto.getDados(), headers, HttpStatus.OK);
+	}
+
+	@PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	public UsuarioDTO inserir(@RequestPart MultipartFile file, @RequestPart Usuario usuario) throws IOException {
+		return usuarioService.inserir(usuario, file);
+	}
+
+	//////////////////////
 
 	// MOSTRA TODOS OS USUARIOS
 	@GetMapping
--- src/main/java/org/serratec/backend/redesocial/service/UsuarioService.java
+++ src/main/java/org/serratec/backend/redesocial/service/UsuarioService.java
@@ -1,11 +1,18 @@
 package org.serratec.backend.redesocial.service;
 
+import org.serratec.backend.redesocial.DTO.UsuarioDTO;
 import org.serratec.backend.redesocial.model.Usuario;
 import org.serratec.backend.redesocial.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +21,53 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+	@Autowired
+	private FotoService fotoService;
+	
+	public List<UsuarioDTO> listar() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		List<UsuarioDTO> usuariosDTO = new ArrayList<>();
+		
+		usuarios.forEach(f -> {
+			usuariosDTO.add(adicionarImagemUrl(f));
+		});
+		
+		return usuariosDTO;
+	}
+	
+	public UsuarioDTO buscar(Long id) {
+		Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+		return adicionarImagemUrl(usuarioOpt.get());
+	}
+	
+	public UsuarioDTO inserir(Usuario usuario, MultipartFile file) throws IOException {
+		usuario = usuarioRepository.save(usuario);
+		fotoService.inserir(usuario, file);
+		return adicionarImagemUrl(usuario);
+	}
+	
+	
+	public UsuarioDTO adicionarImagemUrl(Usuario usuario) {
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentContextPath()
+				.path("/usuarios/{id}/foto")
+				.buildAndExpand(usuario.getId())
+				.toUri();
+		
+		UsuarioDTO dto = new UsuarioDTO();
+		dto.setNome(usuario.getNome());
+		dto.setEmail(usuario.getEmail());
+		dto.setUrl(uri.toString());
+		dto.setDataNascimento(usuario.getDataNascimento());
+		//dto.setPerfis(usuario.getPerfis());
+		
+		return dto;
+	}
+	
+	//////////////////////
+    
+    
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
