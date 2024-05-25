2f7d58d29aacb7cf53e69252ec4545f5e997c607
LTilio <Leandro.tilio@gmail.com>
2024-05-25 19:17:34 -0300

    Fix: Ajuste em indentação

--- src/main/java/org/serratec/backend/redesocial/service/PerfilService.java
+++ src/main/java/org/serratec/backend/redesocial/service/PerfilService.java
@@ -3,12 +3,10 @@ package org.serratec.backend.redesocial.service;
 import java.util.Optional;
 
 import org.serratec.backend.redesocial.model.Perfil;
-import org.serratec.backend.redesocial.model.UsuarioPerfil;
-import org.serratec.backend.redesocial.model.UsuarioPerfilPK;
 import org.serratec.backend.redesocial.repository.PerfilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class PerfilService {
@@ -19,13 +17,6 @@ public class PerfilService {
 	public Perfil buscar(Long usuarioPerfilPK) {
 		Optional<Perfil> perfilOpt = perfilRepository.findById(usuarioPerfilPK);
 		return perfilOpt.get();
-	}
-	
-	
-	// DANDO MERDA, RESOLVER ISSO
-	public UsuarioPerfil buscar(UsuarioPerfilPK id) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 	
 }
